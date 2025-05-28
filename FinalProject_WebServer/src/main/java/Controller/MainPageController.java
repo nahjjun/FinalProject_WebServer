03693f4d388745b56e1edd93156457ddc8a16fbe
nahjjun <nahjjun1125@naver.com>
2025-05-28 15:09:54 +0900

    05/28 15:09 나형준 로그아웃 구현완료. header 따로 분리해서 if 문으로 gui 구분함

--- FinalProject_WebServer/src/main/java/Controller/MainPageController.java
+++ FinalProject_WebServer/src/main/java/Controller/MainPageController.java
@@ -20,15 +20,11 @@ public class MainPageController extends HttpServlet {
 
 	// 메인 페이지 보내주는 함수
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		// 현재 세션을 확인해서, 로그인이 되어있는지 확인한다. 
-		HttpSession session = request.getSession(); // request.getSession()를 하면 기존 세션이 없으면 새로운 세션을 만들어서 반환하고, 있다면 기존 세션을 반환한다. 
-		LoginUser loginUser = null;
-		RequestDispatcher dispatcher = null;
-		if((loginUser = (LoginUser)session.getAttribute("loginUser")) == null) { // 사용자 로그인 정보가 저장이 안되어 있는 경우, 로그인 전 mainPage를 보내준다.  
-			dispatcher = request.getRequestDispatcher("/mainPage_logouted.jsp");
-		} else {
-		
+		response.setContentType("text/html; charset=UTF-8"); 
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/mainPage.jsp");;
+		String logout = request.getParameter("logout");
+		if("1".equals(logout)) { // 로그아웃 된 상태라면 알림창 보내기 (null exception을 일으키지 않기 위해 "1".equals() 형태로 사용함 
+			request.setAttribute("errorScript", "<script>alert('로그아웃 되었습니다!');</script>");
 		}
 		dispatcher.forward(request, response);
 	}
--- FinalProject_WebServer/src/main/webapp/footer.jsp
+++ FinalProject_WebServer/src/main/webapp/footer.jsp
@@ -1,3 +1,5 @@
  <footer class="pt-3 mt-4 text-body-secondary border-top">
       &copy; SKUNIV SKV. All Rights Reserved
  </footer>
+ <!-- 경고 팝업창 -->
+${errorScript}
